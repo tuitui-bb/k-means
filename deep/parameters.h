c3d8d743c0536b8ca68e45dde69ab7a76754e222
Miss <Miss@LAPTOP-I5NF3GMU>
2021-01-13 23:00:09 +0800

    添加项目文件。

---
+++ deep/parameters.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+//选择需要演示的demo
+#define DEMO_METHOD		1			//0:yolov3 demo	1:openpose demo	
+
+//参数设置
+#define YOLOV3_VIDEO		"D:\\vtest.avi"		
+#define OPENPOSE_VIDEO		"D:\\openposeTest.mp4"		
+
+//#define INPUT_METHOD	
